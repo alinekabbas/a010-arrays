2e94eda69a926e785ce72add34c9969cbce150b4
alinekabbas <alinekabbas@gmail.com>
2022-09-01 12:44:26 -0300

    AmmalA-aline-kabbas

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const apenasNumeros = [74, 56, 9, 18, 47]
+const apenasString = ["café", "leite", "pão"]
+const tudoJunto = [31, 4, "goiaba", "maça", 20 > 30]
+
+console.log(apenasNumeros)
+console.log(apenasString)
+console.log(tudoJunto)
